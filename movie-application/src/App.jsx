9323b57be6c6ccfacbb7f6a97f1401c3dfef25f7
ashishngrr <nagarashish1806@gmail.com>
2023-05-24 22:06:23 +0530

    basic setup for movies app

---
+++ movie-application/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState , useEffect} from 'react';
+import {fetchDataFromApi} from "./utils/api";
+import { useSelector, useDispatch } from 'react-redux';
+
+import { getApiConfiguration } from './store/homeSlice';
+
+
+function App() {
+
+  const dispatch = useDispatch();
+  const {url} = useSelector((state)=> 
+  state.home)
+  console.log(url)
+
+  useEffect(()=>{
+    apiTesting()
+  },[])
+  const apiTesting = () => {
+    fetchDataFromApi('/movie/popular')
+      .then((res)=> {
+        console.log("response", res)
+        dispatch(getApiConfiguration(res))
+      })
+  }
+
+  return (
+    <div className='App'>
+        App
+        {url?.total_pages}
+    </div>
+  )
+}
+
+export default App
