bd01d6c89d2f2dcabf8fe4b267b4719f3a4c176a
ashishngrr <nagarashish1806@gmail.com>
2023-05-29 22:31:38 +0530

    add herro banner component

--- movie-application/src/App.jsx
+++ movie-application/src/App.jsx
@@ -1,9 +1,16 @@
 import { useState , useEffect} from 'react';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import {fetchDataFromApi} from "./utils/api";
 import { useSelector, useDispatch } from 'react-redux';
 
 import { getApiConfiguration } from './store/homeSlice';
-
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from './pages/home/Home';
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
 
 function App() {
 
@@ -13,21 +20,34 @@ function App() {
   console.log(url)
 
   useEffect(()=>{
-    apiTesting()
+    fetchApiConfig()
   },[])
-  const apiTesting = () => {
-    fetchDataFromApi('/movie/popular')
+  const fetchApiConfig = () => {
+    fetchDataFromApi('/configuration')
       .then((res)=> {
         console.log("response", res)
-        dispatch(getApiConfiguration(res))
+        const url = {
+          backdrop: res.images.secure_base_url + "original", 
+          poster: res.images.secure_base_url + "original", 
+          profile: res.images.secure_base_url + "original", 
+
+        }
+        dispatch(getApiConfiguration(url))
       })
   }
 
   return (
-    <div className='App'>
-        App
-        {url?.total_pages}
-    </div>
+    <BrowserRouter>
+    <Header />
+      <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/:mediaType/:id' element={<Details />}/>
+          <Route path='/search/:query' element={<SearchResult />}/>
+          <Route path='/explore/:mediaType' element={<Explore />}/>
+          <Route path='*' element={<PageNotFound />}/>
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
   )
 }
 
