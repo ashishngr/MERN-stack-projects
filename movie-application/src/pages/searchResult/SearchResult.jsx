1d197e2aeb5151cc4eab23000aaaab38f9d0e7b2
ashishngrr <nagarashish1806@gmail.com>
2023-06-15 22:14:40 +0530

    adding search funtionality

--- movie-application/src/pages/searchResult/SearchResult.jsx
+++ movie-application/src/pages/searchResult/SearchResult.jsx
@@ -3,19 +3,22 @@ import {useParams} from "react-router-dom";
 import InfiniteScroll from "react-infinite-scroll-component"; 
 
 import "./style.scss"
-import {fetchDataFromApi} from "../../utils/api";
+import { fetchDataFromApi } from "../../utils/api";
 import ConntentWrapper from "../../contentWrapper/ContentWrapper";
 
 // import MovieCard from "../../components/movieCard/MovieCard";
-// import Spinner from "../../components/spinner/Spinner";
+import Spinner from "../../components/spinner/Spinner";
+
 import noResults from "../../assets/no-results.png";
+import ContentWrapper from "../../contentWrapper/ContentWrapper";
+import MovieCard from "../../components/movieCard/MovieCard";
 
 
 const SearchResult = () => {
   const [data, setData] = useState(null);
   const [pageNum, setPageNum] = useState(1);
   const [loading, setLoading] = useState(false);
-  const {query} = useParams();
+  const { query } = useParams();
 
   const fetchInitialData = () => {
     setLoading(true);
@@ -26,21 +29,65 @@ const SearchResult = () => {
     })
   }
 
-  // const fetchNextPageData =() => {
-  //   fetchDataFromApi(`/search/multi?query=${query}&page=${pageNum}`).then((res)=>{
-      
-  //   })
-  // }
+  const fetchNextPageData =() => {
+    fetchDataFromApi(`/search/multi?query=${query}&page=${pageNum}`).then((res)=>{
+      if(data?.results){
+        setData({
+          ...data, results: [...data?.results, ...res.results]
+        })
+      }else{
+        setData(res)
+      }
+      setPageNum((prev)=> prev + 1)
+    })
+  }
 
   useEffect(()=>{
-    fetchInitialData
+    fetchInitialData();
   },[query])
 
 
   return (
-    <div className="searchResultPage">
-      SearchResult
-    </div>
+    <div className="searchResultsPage">
+    {loading && <Spinner initial={true} />}
+    {!loading && (
+        <ContentWrapper>
+            {data?.results?.length > 0 ? (
+                <>
+                    <div className="pageTitle">
+                        {`Search ${
+                            data?.total_results > 1
+                                ? "results"
+                                : "result"
+                        } of '${query}'`}
+                    </div>
+                    <InfiniteScroll
+                        className="content"
+                        dataLength={data?.results?.length || []}
+                        next={fetchNextPageData}
+                        hasMore={pageNum <= data?.total_pages}
+                        loader={<Spinner />}
+                    >
+                        {data?.results.map((item, index) => {
+                            if (item.media_type === "person") return;
+                            return (
+                                <MovieCard
+                                    key={index}
+                                    data={item}
+                                    fromSearch={true}
+                                />
+                            );
+                        })}
+                    </InfiniteScroll>
+                </>
+            ) : (
+                <span className="resultNotFound">
+                    Sorry, Results not found!
+                </span>
+            )}
+        </ContentWrapper>
+    )}
+</div>
   )
 }
 
